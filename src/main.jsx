5406ade34818a5865c696ef82897a675bcc0fe8b
FRANCISCO RODRIGO VIEIRA NOGUEIRA <francisconogueira8987@digitalcollege.internal>
2025-09-02 20:17:40 -0300

    Confugurando as rotas

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,19 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import 'bootstrap'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Login } from './pages/Login.jsx'
+import { Cadastro } from './pages/Cadastro.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+    {/*<App /> */}
+    <Routes>
+      <Route path='/' element={<App />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/cadastro' element={<Cadastro />} />
+    </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
