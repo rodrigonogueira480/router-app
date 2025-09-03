6fe892d0061998f9bf63acbc1e9d454fe9125077
FRANCISCO RODRIGO VIEIRA NOGUEIRA <francisconogueira8987@digitalcollege.internal>
2025-09-02 21:53:12 -0300

    tela de login do bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -3,21 +3,33 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import 'bootstrap'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, createBrowserRouter, Route, RouterProvider, Routes } from 'react-router-dom'
 import { Login } from './pages/Login.jsx'
 import { Cadastro } from './pages/Cadastro.jsx'
 import { NotFound } from './pages/NotFound.jsx'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: '/home',
+    element: <App />
+  },
+  {
+    path: '/cadastro',
+    element: <Cadastro />
+  },
+  {
+    path: '*',
+    element: <NotFound/>
+  },
+]);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-    {/*<App /> */}
-    <Routes>
-      <Route path='/' element={<Login />} />
-      <Route path='/home' element={<App />} />
-      <Route path='/cadastro' element={<Cadastro />} />
-      <Route path='*' element={<NotFound />} />
-    </Routes>
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </StrictMode>,
 )
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,35 @@
 import React from 'react'
 
 export const Login = () => {
-  return (
-    <div>Login</div>
-  )
+
+const login = () => {
+    /*Evita recarregar a página*/ 
+    //e.preventDefault();
+    console.log('logou');
+}
+
+    return (
+         <main className="form-signin w-100 m-auto">
+<img className="mb-4" src="	https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57" />
+            <form onSubmit={login}>
+                <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+                <div className="form-floating">
+                    <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
+                    <label htmlFor="floatingInput">Email address</label>
+                </div>
+                <div className="form-floating">
+                    <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
+                    <label htmlFor="floatingPassword">Password</label>
+                </div>
+                <div className="form-check text-start my-3">
+                    <input className="form-check-input" type="checkbox" value="remember-me" id="checkDefault" />
+                    <label className="form-check-label" htmlFor="checkDefault">
+                        Remember me
+                    </label>
+                </div>
+                <button className="btn btn-primary w-100 py-2" type="submit">Sign in</button>
+                <p className="mt-5 mb-3 text-body-secondary">© 2017–2025</p>
+            </form>
+        </main>
+    )
 }
